cd8bd2021ea20b966d9e3663cfe9021bfc428c5d
yegor256 <yegor256@gmail.com>
2021-07-07 23:53:36 +0300

    typo

--- discover-repos.rb
+++ discover-repos.rb
@@ -29,10 +29,10 @@ require 'octokit'
 
 opts = Slop.parse do |o|
   o.integer '--total', 'Total number of repos to take from GitHub', required: true
-  o.integer '--min-stars', 'Minimum GitHub stars in each repo', required: true, default: 1000
-  o.integer '--max-stars', 'Maximum GitHub stars in each repo', required: true, default: 10000
-  o.integer '--min-size', 'Minimum size of GitHub repo, in Kb', required: true, default: 200
-  o.integer '--max-size', 'Maximum size of GitHub repo, in Kb', required: true, default: 10000
+  o.integer '--min-stars', 'Minimum GitHub stars in each repo', default: 1000
+  o.integer '--max-stars', 'Maximum GitHub stars in each repo', default: 10000
+  o.integer '--min-size', 'Minimum size of GitHub repo, in Kb', default: 200
+  o.integer '--max-size', 'Maximum size of GitHub repo, in Kb', default: 10000
   o.string '--path', 'The file name to save the list to', required: true
   o.on '--help' do
     puts o
