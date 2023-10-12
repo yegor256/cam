829f04d228724dc058ac8705ba1435fc224e9964
Yegor Bugayenko <yegor256@gmail.com>
2023-10-12 15:29:40 +0300

    #105 report pmd

--- tests/metrics/test-pmd.sh
+++ tests/metrics/test-pmd.sh
@@ -28,6 +28,6 @@ stdout=$2
 
 java="${temp}/Foo long 'weird' name (--).java"
 echo "class Foo {}" > "${java}"
-"${LOCAL}/metrics/pmd.sh" "${java}" "${temp}/stdout"
+"${LOCAL}/metrics/pmd.sh" "${java}" "${temp}/stdout" > "${stdout}" 2>&1
 grep "coc 0" "${temp}/stdout" > "${stdout}" 2>&1
 echo "👍🏻 Correctly calculated congitive complexity"
