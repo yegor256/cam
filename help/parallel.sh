430f6be0599bfc1dbff251a1badd9b141c19ddc4
Yegor Bugayenko <yegor256@gmail.com>
2024-02-22 16:48:40 +0300

    cores

--- help/parallel.sh
+++ help/parallel.sh
@@ -27,12 +27,13 @@ jobs=$1
 x=$2
 if [ -z "${x}" ]; then x=1; fi
 
+cores=$(echo "$(nproc) * ${x}" | bc)
 args=(
   '--halt-on-error=now,fail=1'
   '--halt=now,fail=1'
   '--retries=3'
-  '--load=8'
-  "--max-procs=$(echo "$(nproc) * ${x}" | bc)"
+  "--load="${cores}"
+  "--max-procs=${cores}"
 )
 
 uniq "${jobs}" | parallel "${args[@]}" "${SHELL}" -c
