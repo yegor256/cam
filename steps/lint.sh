39418c6d2e48e344e96a7de6e4a47211d683c0f4
Bogdan Savko <kernbldhf@gmail.com>
2024-03-20 22:42:38 +0300

    patch gitignore and rename custom plugin

--- steps/lint.sh
+++ steps/lint.sh
@@ -38,7 +38,7 @@ flake8 --max-line-length=140 "${LOCAL}/"
 
 PYTHONPATH="${PYTHONPATH}:${LOCAL}/pylint_plugins/"
 
-find "${LOCAL}" -type f -name '*.py' -print0 | xargs -0 -n1 pylint --enable-all-extensions --load-plugins=const_checker \
+find "${LOCAL}" -type f -name '*.py' -print0 | xargs -0 -n1 pylint --enable-all-extensions --load-plugins=custom_checkers \
     --disable=empty-comment \
     --disable=missing-module-docstring \
     --disable=invalid-name \
