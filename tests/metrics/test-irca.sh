492a827ff213d473d8ddffc6aec9ab967136f6d5
zaqbez39me <korolov_k@mail.ru>
2024-04-16 18:13:30 +0300

    disabled shellcheck SC2317

--- tests/metrics/test-irca.sh
+++ tests/metrics/test-irca.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-
+# shellcheck disable=SC2317
+# TODO: REMOVE SHELLCHECK DISABLE RIGHT AFTER IMPLEMENTING ir.sh
 # The MIT License (MIT)
 #
 # Copyright (c) 2021-2024 Yegor Bugayenko
