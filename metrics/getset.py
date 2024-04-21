8721b7f2d14dece1bd0d90b1d2c755bac8acea2b
padjal <dzhalev.pavel@gmail.com>
2024-04-21 12:57:29 +0300

    Fix all linting errors

--- metrics/getset.py
+++ metrics/getset.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
 import sys
 from javalang import tree, parse
-from typing import Optional, Tuple
 
 sys.setrecursionlimit(10000)
 
 
-def is_getter_or_setter(method: tree.MethodDeclaration) -> Optional[str]:
+def is_getter_or_setter(method: tree.MethodDeclaration) -> str | None:
     """Figures out whether a method is a getter or a setter."""
     if isinstance(method, tree.MethodDeclaration):
         if method.name.startswith("get") and len(method.parameters) == 0:
             return "getter"
-        elif method.name.startswith("set") and len(method.parameters) == 1:
+        if method.name.startswith("set") and len(method.parameters) == 1:
             return "setter"
     return None
 
@@ -25,25 +24,25 @@ def count_branches(node: tree.Node) -> int:
     return count
 
 
-def analyze_method(method: tree.MethodDeclaration) -> Optional[Tuple[str, int, int]]:
+def analyze_method(method: tree.MethodDeclaration) -> tuple[str, int, int] | None:
     """Analyzes the complexity of the method."""
-    method_type = is_getter_or_setter(method)
-    if not method_type:
+    if not (method_type := is_getter_or_setter(method)):
         return None
 
     complexity = 0
     branches = 0
-    for _, node in method.filter(tree.Statement):
-        branches += count_branches(node)
+    for _, statement_node in method.filter(tree.Statement):
+        branches += count_branches(statement_node)
 
         if method_type == "setter":
             # Increase complexity for each additional variable assignment
-            if isinstance(node, tree.Assignment) and node.expressionl != method.parameters[0].name:
+            if isinstance(statement_node, tree.Assignment) and statement_node.expressionl != method.parameters[0].name:
                 complexity += 1
 
         elif method_type == "getter":
             # Increase complexity for each statement not related to the returned variable
-            if not (isinstance(node, tree.ReturnStatement) and node.expression.selectors[0].member == method.name[3:].lower()):
+            if not (isinstance(statement_node, tree.ReturnStatement)
+                    and statement_node.expression.selectors[0].member == method.name[3:].lower()):
                 complexity += 1
 
     # Increase complexity for branching
@@ -53,18 +52,16 @@ def analyze_method(method: tree.MethodDeclaration) -> Optional[Tuple[str, int, i
 
 
 if __name__ == '__main__':
-    JAVA = sys.argv[1]
-    METRICS = sys.argv[2]
-    with open(JAVA, encoding='utf-8', errors='ignore') as f:
+    java = sys.argv[1]
+    metrics = sys.argv[2]
+    with open(java, encoding='utf-8', errors='ignore') as f:
         try:
             ast = parse.parse(f.read())
-            for _, node in ast.filter(tree.MethodDeclaration):
-                result = analyze_method(node)
-                if result:
-                    method_type, complexity, branches = result
-                    with open(METRICS, 'a', encoding='utf-8') as m:
-                        # m.write(f'{method_type} {complexity} {branches} Branches of {node.name}\n')
-                        m.write(f'Method name: {method_type}; Complexity: {complexity}; Branches: {branches}\n')
+            for _, tree_node in ast.filter(tree.MethodDeclaration):
+                if (result := analyze_method(tree_node)):
+                    method_type_result, complexity_result, branches_result = result
+                    with open(metrics, 'a', encoding='utf-8') as m:
+                        m.write(f'Method name: {method_type_result}; Complexity: {complexity_result}; Branches: {branches_result}\n')
         except FileNotFoundError as exception:
-            message = f"{type(exception).__name__} {str(exception)}: {JAVA}"
+            message = f"{type(exception).__name__} {str(exception)}: {java}"
             sys.exit(message)
