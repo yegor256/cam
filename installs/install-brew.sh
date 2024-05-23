79e803c4e0e7040eeb256fb7de8208f7529049c3
Timur Harin <timurx404@gmail.com>
2024-05-24 00:35:42 +0300

    add brew and wget install

--- installs/install-brew.sh
+++ installs/install-brew.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/env bash
-# The MIT License (MIT)
+#!/bin/bash
+# MIT License
 #
 # Copyright (c) 2021-2024 Yegor Bugayenko
 #
@@ -10,22 +10,36 @@
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
 #
-# The above copyright notice and this permission notice shall be included
-# in all copies or substantial portions of the Software.
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+
 set -e
 set -o pipefail
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  "${LOCAL}/install-brew.sh"
-  exit 0
-else
-  exit 1
+if brew -v; then
+    echo "Homebrew is already installed"
+    exit
 fi
+
+tmp=$(mktemp -d)
+mkdir -p "${tmp}"
+cd "${tmp}"
+
+if wget -h; then 
+  echo "wget is already installed"
+else
+  brew install wget
+fi
+
+wget --quiet https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+chmod +x install.sh
+./install.sh
+
