99cf6bf9a5383f2baa7bba3c648ec089a29247ab
h1alexbel <aliaksei.bialiauski@hey.com>
2024-07-01 18:21:26 +0300

    feat(#228): fetch config

--- steps/discover-repos.rb
+++ steps/discover-repos.rb
@@ -118,18 +118,29 @@ end
 
 puts 'Not searching GitHub API, using mock repos' if opts[:dry]
 
-def fetch(github, licenses, opts, page, query, size)
-  if opts[:dry]
-    mock_reps(page, size, licenses)
+def fetch(config)
+  if config[opts][:dry]
+    mock_reps(config[:page], config[:size], config[:licenses])
   else
-    github.search_repositories(query, per_page: size, page: page)
+    config[:github].search_repositories(
+      [config: query], per_page: config[:size], page: config[:page]
+    )
   end
 end
+
+config = {
+  github: github,
+  licenses: licenses,
+  opts: opts,
+  page: page,
+  query: query,
+  size: size
+}
 
 loop do
   break if page * size > max
   count = 0
-  json = fetch(github, licenses, opts, page, query, size)
+  json = fetch(config)
   json[:items].each do |i|
     no_license = i[:license].nil? || !licenses.include?(i[:license][:key])
     puts "Repo #{i[:full_name]} doesn't contain required license. Skipping" if no_license
