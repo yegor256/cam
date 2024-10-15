8cfc7d400dcff471a6db4604424761f048188f43
Aleksandr Ryabov <a.ryabov@innopolis.university>
2024-10-15 13:36:45 +0300

    Add bug discovering metric

---
+++ tests/metrics/test-bug_discover.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+# The MIT License (MIT)
+#
+# Copyright (c) 2021-2024 Yegor Bugayenko
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+set -e
+set -o pipefail
+
+temp=$1
+stdout=$2
+
+{
+    pmd --version
+    xmllint --version
+} > "${stdout}" 2>&1
+echo "👍🏻 PMD dependencies are installed"
+
+{
+    java="${temp}/Foo long 'weird' name (--).java"
+    echo "
+    public class StaticField {
+        static int x;
+        public FinalFields(int y) {
+            x = y; // unsafe
+        }
+    }" > "${java}"
+    "${LOCAL}/metrics/bug_discover.sh" "${java}" "${temp}/stdout"
+    grep "BugNum 1 " "${temp}/stdout"
+} > "${stdout}" 2>&1
+echo "👍🏻 Correctly calculates the bug discovery metric"
