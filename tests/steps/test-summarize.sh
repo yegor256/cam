54f3eba0ce894f5c2b6cfcf2a3162b9bcf399c67
Alexander Afonin <63101279+lueFlake@users.noreply.github.com>
2024-10-16 16:42:42 +0300

    test strange file and repo names

--- tests/steps/test-summarize.sh
+++ tests/steps/test-summarize.sh
@@ -24,6 +24,8 @@ set -e
 set -o pipefail
 
 stdout=$2
+file_name="file -p \(test \) \\n\?.k"
+repo_name="comp_.lex\$repo\[info\]>"
 
 {
     echo "${TARGET}"
@@ -31,60 +33,59 @@ stdout=$2
     rm -rf "${TARGET}/measurements"
     rm -rf "${TARGET}/summary"
     rm -rf "${TARGET}/temp"
-    dir1="${TARGET}/measurements/repo1"
+    dir1="${TARGET}/measurements/${repo_name}1"
     mkdir -p "${dir1}"
     "${LOCAL}/steps/summarize.sh"
     test -z "$(ls -A "${TARGET}/data/summary")"
 } > "${stdout}" 2>&1
-echo "👍🏻 Summarization step handled empty repository correctly"
+echo "👍🏻 Summarization step handled empty ${repo_name}sitory correctly"
 
 {
     rm -rf "${TARGET}/data"
     rm -rf "${TARGET}/measurements"
     rm -rf "${TARGET}/summary"
     rm -rf "${TARGET}/temp"
-    dir1="${TARGET}/measurements/repo1"
+    dir1="${TARGET}/measurements/${repo_name}1"
     mkdir -p "${TARGET}/temp"
     touch "${TARGET}/temp/repos-to-aggregate.txt"
-    echo "repo1" >> "${TARGET}/temp/repos-to-aggregate.txt"
+    echo "${repo_name}1" >> "${TARGET}/temp/repos-to-aggregate.txt"
     mkdir -p "${dir1}"
-    echo "50" > "${dir1}/file1.m.LOC"
-    echo "100" > "${dir1}/file2.m.LOC"
-    echo "10" > "${dir1}/file1.m.CYC"
-    echo "20" > "${dir1}/file2.m.CYC"
+    echo "50" > "${dir1}/${file_name}1.m.LOC"
+    echo "100" > "${dir1}/${file_name}2.m.LOC"
+    echo "10" > "${dir1}/${file_name}1.m.CYC"
+    echo "20" > "${dir1}/${file_name}2.m.CYC"
     "${LOCAL}/steps/summarize.sh"
     test -e "${TARGET}/data/summary/LOC.csv"
     test -e "${TARGET}/data/summary/CYC.csv"
-    grep "repo1,2,150" < "${TARGET}/data/summary/LOC.csv"
-    grep "repo1,2,30" < "${TARGET}/data/summary/CYC.csv"
+    grep "${repo_name}1,2,150" < "${TARGET}/data/summary/LOC.csv"
+    grep "${repo_name}1,2,30" < "${TARGET}/data/summary/CYC.csv"
 } > "${stdout}" 2>&1
 echo "👍🏻 Summarization step handled multiple metrics correctly"
 
-
 {
     rm -rf "${TARGET}/data"
     rm -rf "${TARGET}/measurements"
     rm -rf "${TARGET}/summary"
     rm -rf "${TARGET}/temp"
-    dir1="${TARGET}/measurements/repo1"
+    dir1="${TARGET}/measurements/${repo_name}1"
     mkdir -p "${TARGET}/temp"
     touch "${TARGET}/temp/repos-to-aggregate.txt"
-    echo "repo1" >> "${TARGET}/temp/repos-to-aggregate.txt"
-    echo "repo2" >> "${TARGET}/temp/repos-to-aggregate.txt"
+    echo "${repo_name}1" >> "${TARGET}/temp/repos-to-aggregate.txt"
+    echo "${repo_name}2" >> "${TARGET}/temp/repos-to-aggregate.txt"
     mkdir -p "${dir1}"
-    echo "50" > "${dir1}/file1.m.LOC"
-    echo "100" > "${dir1}/file2.m.LOC"
-    echo "10" > "${dir1}/file1.m.CYC"
-    dir2="${TARGET}/measurements/repo2"
+    echo "50" > "${dir1}/${file_name}1.m.LOC"
+    echo "100" > "${dir1}/${file_name}2.m.LOC"
+    echo "10" > "${dir1}/${file_name}1.m.CYC"
+    dir2="${TARGET}/measurements/${repo_name}2"
     mkdir -p "${dir2}"
-    echo "25" > "${dir2}/file1.m.LOC"
+    echo "25" > "${dir2}/${file_name}1.m.LOC"
     "${LOCAL}/steps/summarize.sh"
     test -e "${TARGET}/data/summary/LOC.csv"
-    grep "repo1,2,150" < "${TARGET}/data/summary/LOC.csv"
-    grep "repo2,1,25" < "${TARGET}/data/summary/LOC.csv"
+    grep "${repo_name}1,2,150" < "${TARGET}/data/summary/LOC.csv"
+    grep "${repo_name}2,1,25" < "${TARGET}/data/summary/LOC.csv"
     test -e "${TARGET}/data/summary/CYC.csv"
-    grep "repo1,1,10" < "${TARGET}/data/summary/CYC.csv"
-    if grep "repo2" < "${TARGET}/data/summary/CYC.csv"; then
+    grep "${repo_name}1,1,10" < "${TARGET}/data/summary/CYC.csv"
+    if grep "${repo_name}2" < "${TARGET}/data/summary/CYC.csv"; then
         exit 1
     fi
 } > "${stdout}" 2>&1
