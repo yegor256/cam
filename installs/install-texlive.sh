f681f28b3b229e1bccec400875b687a9fb6b9e5b
dzhovi <starkizi47@yandex.ru>
2024-12-05 12:25:40 +0300

    changed tlmgr mirror to german

--- installs/install-texlive.sh
+++ installs/install-texlive.sh
@@ -26,7 +26,7 @@ set -o pipefail
 
 if ! "${LOCAL}/help/texlive-bin.sh"; then
   if [ ! -f "install-tl.zip" ]; then
-    wget --quiet http://mirror.ctan.org/systems/texlive/tlnet/install-tl.zip
+    wget --quiet https://mirror.clientvps.com/CTAN/systems/texlive/tlnet/install-tl.zip
   fi
   rm -rf install-tl
   unzip install-tl.zip -d install-tl
